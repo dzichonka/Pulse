4ffc26199a40162e43d85801021fa5ea2f5b52bd
Anna Vasilevich <dzichonka@gmail.com>
2022-11-02 12:44:14 +0100

    slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
 
+    $('.carousel__wrapper').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    arrows: false,
+                    infinite: true,
+                },
+            },
+        ],
+    });
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -213,21 +233,8 @@ $(document).ready(function () {
 
 
 
-    // //     $('.carousel__wrapper').slick({
-    // //         speed: 1000,
-    // //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
-    // //         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
-    // //         responsive: [
-    // //             {
-    // //                 breakpoint: 768,
-    // //                 settings: {
-    // //                     arrows: false,
-    // //                     dots: true,
-    // //                 }
-    // //             }
-    // //         ],
-    // //     });
-    // // });
+
+    //   });
 
     // var slider = tns({
     //     container: '.carousel__wrapper',
